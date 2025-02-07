6aefc412c10f69cd39778d082962160cfe176d41
4nh31 <angelyupit1@outlook.es>
2025-02-07 16:07:22 -0500

    se agrego la config a la bd

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const http = require('http');
+const express = require('express');
+const connection = require('./src/config/db.js'); 
 
-const server = http.createServer((req, res) =>{
+const app = express();
+const PORT = 3000;
 
-    
+// Ruta de ejemplo
+app.get('/usuarios', (req, res) => {
+    connection.query('SELECT * FROM usuario', (err, results) => {
+      if (err) {
+        return res.status(500).send('Error en la base de datos');
+      }
+      res.json(results);
+    });
+  });
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-server.listen(3000, ()=>console.log('Servidor corriendo en http://localhost:3000'));
---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2'); // O el driver de tu base de datos
+require('dotenv').config(); // Cargar variables de entorno
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error conectando a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión exitosa a la base de datos');
+});
+
+module.exports = connection;
