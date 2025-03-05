62d120ced2a84595c1c78c5527f5979f35b8920e
4nh31 <angelyupit1@outlook.es>
2025-03-05 09:05:46 -0500

    endpoints segovia

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const userRoutes = require('./routes/usuarioRutas');
+const pedidoRoutes = require('./routes/pedidoRutas');
+const swaggerUI = require('swagger-ui-express');
+//import swaggerUI from "swagger-ui-express";
+
 
 const app = express();
-app.use(express.json()); // Para leer JSON en las peticiones
-app.use('/usuarios', userRoutes);
+app.use(express.json());
+app.use("api-docs",swaggerUI.serve, swaggerUI.setup())
 
+app.use('/usuarios', userRoutes);
+app.use('/pedidos', pedidoRoutes);
+
+console.log("Swagger disponible en http://localhost:3000/api-docs");
 app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
---
+++ swagger/swagger.js
@@ -0,0 +1,24 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API de E-commerce",
+      version: "1.0.0",
+      description: "Documentación de la API de E-commerce con Swagger",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000", // url del servidor 
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // Ruta de archvios donde se definen los endpoints
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+
+module.exports = setupSwagger;
